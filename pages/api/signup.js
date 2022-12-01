c4c1a82c60a9611470c9df98fe41fcd08b7a1389
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-12-01 19:58:19 +0900

    error message signup missing fields

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -14,14 +14,22 @@ export default async function signup(req, res) {
     const db = await openDb()
 
     if (req.method === "POST") {
+        if (!req.body.name || !req.body.email || !req.body.password) {
+            res.status(400).json({ message: "Missing fields", success: false})
+            return
+        }
         hash(req.body.password, 12, async function (err, hash) {
             const statement = await db.prepare(
                 "INSERT INTO Person (name, email, password) VALUES (?, ?, ?)"
             )
+
             await statement.run(req.body.name, req.body.email, hash)
             res.json({ message: "Signup success", success: true })
         })
     } else {
-        res.status(405).json({ message: "We only support POST", success: false })
+        res.status(405).json({
+            message: "We only support POST",
+            success: false,
+        })
     }
 }
