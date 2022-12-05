2dcb8c1de5815d25e85cf4c08ba10dc2e3b8f504
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-12-05 10:29:02 +0900

    make animation faster

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import styles from "../styles/index.module.scss"
 import Head from "next/head"
 import SearchBar from "../components/SearchBar"
 import Logo from "../components/Logo"
-import Slogan from "../components/Slogan"
 import Header from "../components/Header"
 import isLoggedIn from "./api/isLoggedIn"
 import Router from "next/router"
@@ -63,7 +62,6 @@ export default function Home({ cookie }) {
                 <title>Charity search</title>
             </Head>
             <Logo />
-            <Slogan />
             <div className={styles.searchbar}>
                 <SearchBar isLoggedIn={cookie} />
             </div>
