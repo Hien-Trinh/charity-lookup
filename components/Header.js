128b9bbcc3353999acecf608a71a39d93404f486
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-12-05 10:22:40 +0900

    back to home button

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import styles from "./Header.module.scss"
 import SearchBar from "./SearchBar"
 import Router from "next/router"
+import { Tooltip, Button } from "@nextui-org/react"
+import { Left } from "./ArrowIcon"
 
 export default function Header({ isSearchBar, isLoggedIn }) {
     async function handleClick() {
@@ -15,15 +17,30 @@ export default function Header({ isSearchBar, isLoggedIn }) {
         Router.push("/login")
     }
 
+    function handlePrev() {
+        Router.push("/")
+    }
+
     return (
         <div
             className={`${styles.container} ${
                 isSearchBar ? null : styles.animate
             }`}
         >
+            {isSearchBar ? <div className={styles.left}>
+                <Tooltip content={"Back"} color="invert" placement="bottomEnd">
+                    <Button
+                        auto
+                        icon={<Left />}
+                        color="invert"
+                        onPress={handlePrev}
+                    ></Button>
+                </Tooltip>
+            </div> : null}
+
             {isSearchBar ? (
                 <div className={styles.SearchBar}>
-                    <SearchBar isLoggedIn={ isLoggedIn } />
+                    <SearchBar isLoggedIn={isLoggedIn} />
                 </div>
             ) : null}
             <button className={styles.button} onClick={handleClick}>
